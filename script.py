d973d5ac1d5e20dfbf0aca04c9320b6458d5161e
Benson Tran <benson.trn@gmail.com>
2018-10-09 18:35:41 -0400

    inital commit

---
+++ script.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import arrow
+import datetime
+import pandas as pd
+import numpy as np
+
+
+def get_quote_data(symbol='AAPL', data_range='7d', data_interval='1m'):
+    res = requests.get(
+        'https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={data_range}&interval={data_interval}'.format(
+            **locals()))
+    data = res.json()
+    body = data['chart']['result'][0]
+    dt = datetime.datetime
+    dt = pd.Series(map(lambda x: arrow.get(x).to('EST').datetime.replace(tzinfo=None), body['timestamp']), name='dt')
+    df = pd.DataFrame(body['indicators']['quote'][0], index=dt)
+    dg = pd.DataFrame(body['timestamp'])
+    df = df.loc[:, ('close', 'volume')]
+    df.dropna(inplace=True)  # removing NaN rows
+    df.columns = ['CLOSE', 'VOLUME']  # Renaming columns in pandas
+    df.to_csv('out.csv')
+
+    return df
+
+
+data = get_quote_data('AAPL', '7d', '1m')
+print(data)
